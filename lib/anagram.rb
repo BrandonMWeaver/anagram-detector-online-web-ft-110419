429069152334ebc221200cc2602da0ae86e184c4
Brandon Weaver <a7xncob@gmail.com>
2019-11-10 01:49:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,14 @@
 class Anagram
+  attr_accessor :words
   
+  def initialize(words)
+    @words = words
+  end
+  
+  def match
+    split_words = []
+    @words.each do |word|
+      split_words << word.split
+    end
+  end
 end
