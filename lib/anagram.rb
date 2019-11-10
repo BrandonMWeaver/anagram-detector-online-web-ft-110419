5dd14b2069113729a7914112bc2b95d34dd229de
Brandon Weaver <a7xncob@gmail.com>
2019-11-10 01:59:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,6 +6,12 @@ class Anagram
   end
   
   def match(words)
-    
+    anagrams = []
+    split_word = @word.split('').sort
+    words.each do |comparison|
+      split_comparison = comparison.split('').sort
+      anagrams << comparison if split_comparison == split_word
+    end
+    return anagrams
   end
 end
